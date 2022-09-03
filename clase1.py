56bdcaad327021c3c2358f954829aa5e2b33b2a3
francis24325 <frantopmising2@gmail.com>
2022-09-03 16:33:58 -0300

    primer comit

---
+++ clase1.py
@@ -0,0 +1,36 @@
+"""
+juan= "ola guille"
+juan= 23
+  (juan)"""
+"""apellido= 23
+nombre= 'juan '
+ (nombre,apellido)"""
+"""print("bienvenido!!!")
+numero1=input ("inresa tu numero:")
+numero2=input("inresa otro numero:")
+suma=int(numero1)+int(numero2)
+resulta1= (suma/100*85)
+print("resultado1:",resulta1)
+porcentaje1=(suma/100*15)
+resultado2= (suma-porcentaje1)
+print ("resultado2:",resultado2)"""
+"""ejercicioN4
+usuario=int(input ("numero1:"))
+usuario2=int (input ("numero2:"))
+usuario3=int (input ("numero3:"))
+promedio=usuario+usuario2+usuario3
+suma=(promedio)/3
+print("promedio",suma)"""
+"""ejercicioN5
+usuario=input ("ingrese nombre:")
+edad=int(input ("y edad: "))
+edad2=(2122-edad)
+print("este sera el año",usuario ,"en el que vas a cumplir en 100 años:",edad2)"""
+"""ejercicioN6
+peso=int(input ("ingrese peso: "))
+libras=peso*2,2)
+print("pesa en libras:",libras)""""como le saco los parentesis"""
+"""ejercicioN6
+temperatura=int(input ("ingrese temperatura:c"))
+fahrenheit=temperatura*33,8
+print fahrenheit,(" F s/Google")"""
